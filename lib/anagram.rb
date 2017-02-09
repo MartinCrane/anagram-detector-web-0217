02cc17c90ab4d2d156537c26e634ef7d0134a7d2
MartinCrane <martin.crane@gmail.com>
2017-02-08 19:20:23 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
-# Your code goes here!
+class Anagram
+	# attr_accessor :possible_words
+	attr_reader :anagram
+
+	def initialize(anagram)
+		@anagram = anagram
+	end
+
+
+	def match(possible_words)
+
+		# word_array = possible_words.split(" ")
+		anagram_array = []
+		
+		possible_words.each do |element|
+
+			if element.split("").sort == @anagram.split("").sort
+				
+				anagram_array << element
+			else
+			end
+		end
+
+		anagram_array
+	end
+end
+
+# s = Anagram.new("blood")
+# s.match(["agisigjdfk", "afkggkkkfg"])
